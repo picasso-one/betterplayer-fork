43d022fffa5c6a1f93d427a3a91e03825157be63
Piotr Daraszkiewicz <pdaraszkiewicz@codespot.io>
2025-08-05 17:16:36 +0200

    ATV-1870: Fix for VOD player

--- lib/src/core/better_player_controller.dart
+++ lib/src/core/better_player_controller.dart
@@ -881,6 +881,7 @@ class BetterPlayerController {
   }
 
   void _displayPlayNextButton(VideoPlayerValue currentVideoPlayerValue) {
+    bool actionDone = false;
     if (betterPlayerPlayNextVideoConfiguration != null && videoPlayerController?.value.duration != null) {
       if ((currentVideoPlayerValue.position.inMilliseconds >=
               (videoPlayerController!.value.duration!.inMilliseconds -
@@ -889,13 +890,18 @@ class BetterPlayerController {
               (videoPlayerController!.value.duration!.inMilliseconds -
                   betterPlayerPlayNextVideoConfiguration!.showBeforeEndMillis +
                   betterPlayerPlayNextVideoConfiguration!.autoSwitchToNextMillis)) {
+        actionDone = true;
         showNextVideoButton();
       } else if (currentVideoPlayerValue.position.inMilliseconds >
           (videoPlayerController!.value.duration!.inMilliseconds -
               betterPlayerPlayNextVideoConfiguration!.showBeforeEndMillis +
               betterPlayerPlayNextVideoConfiguration!.autoSwitchToNextMillis)) {
+        actionDone = true;
         hideNextVideoButton();
       }
+    }
+    if (!actionDone) {
+      _showNextVideoButton = false;
     }
   }
 
