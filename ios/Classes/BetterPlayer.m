bd6e68c24332b3e292433c805374d066f6959b27
RBajor <158453587+RBajor@users.noreply.github.com>
2025-10-08 11:37:47 +0200

    ATV-1914: Fix change speed up playback
    ATV-1914: Fix change speed up playback

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -663,32 +663,19 @@ if (_overriddenDuration > 0 && duration > _overriddenDuration && !CMTIME_IS_INDE
 }
 
 - (void)setSpeed:(double)speed result:(FlutterResult)result {
-    if (speed == 1.0 || speed == 0.0) {
-        _playerRate = 1;
-        result(nil);
-    } else if (speed < 0 || speed > 2.0) {
+    if (speed <= 0.0 || speed > 2.0) {
         result([FlutterError errorWithCode:@"unsupported_speed"
-                                   message:@"Speed must be >= 0.0 and <= 2.0"
+                                   message:@"Speed must be > 0.0 and <= 2.0"
                                    details:nil]);
-    } else if ((speed > 1.0 && _player.currentItem.canPlayFastForward) ||
-               (speed < 1.0 && _player.currentItem.canPlaySlowForward)) {
-        _playerRate = speed;
-        result(nil);
-    } else {
-        if (speed > 1.0) {
-            result([FlutterError errorWithCode:@"unsupported_fast_forward"
-                                       message:@"This video cannot be played fast forward"
-                                       details:nil]);
-        } else {
-            result([FlutterError errorWithCode:@"unsupported_slow_forward"
-                                       message:@"This video cannot be played slow forward"
-                                       details:nil]);
-        }
+        return;
     }
 
-    if (_isPlaying){
+    _playerRate = speed;
+    if (_isPlaying) {
         _player.rate = _playerRate;
     }
+
+    result(nil);
 }
 
 
